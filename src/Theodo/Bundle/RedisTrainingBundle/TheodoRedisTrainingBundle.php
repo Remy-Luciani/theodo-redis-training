681bcfa00523e07e055da2724dd5bdd2711e30d0
Remy Luciani <remyl@theodo.fr>
2013-08-29 00:05:07 +0200

    Add user entity

--- src/Theodo/Bundle/RedisTrainingBundle/TheodoRedisTrainingBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Theodo\Bundle\RedisTrainingBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class TheodoRedisTrainingBundle extends Bundle
-{
-}
--- src/Theodo/Bundle/ScrumOrNotScrumBundle/Controller/DefaultController.php
+++ src/Theodo/Bundle/ScrumOrNotScrumBundle/Controller/DefaultController.php
@@ -4,10 +4,15 @@ namespace Theodo\Bundle\ScrumOrNotScrumBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Theodo\Bundle\ScrumOrNotScrumBundle\Entity\User;
+
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('TheodoScrumOrNotScrumBundle:Default:index.html.twig', array('name' => $name));
+        $user = new User();
+        $user->setName($name);
+
+        return $this->render('TheodoScrumOrNotScrumBundle:Default:index.html.twig', array('user' => $user));
     }
 }
