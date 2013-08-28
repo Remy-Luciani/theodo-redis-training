26df1e139d7ebcdfe8f4925d8d16e76196bf76ba
Remy Luciani <remyl@theodo.fr>
2013-08-29 01:33:22 +0200

    Add finding methods in UserManager

--- src/Theodo/Bundle/ScrumOrNotScrumBundle/Manager/UserManager.php
+++ src/Theodo/Bundle/ScrumOrNotScrumBundle/Manager/UserManager.php
@@ -27,4 +27,22 @@ class UserManager
         $this->redis->set("user:$uid:name", $name);
         $this->redis->set("username:$name:uid", $uid);
     }
+
+    public function find($uid)
+    {
+        $user = new User();
+        $user->setId($uid);
+        $user->setName($this->redis->get("user:$uid:name"));
+
+        return $user;
+    }
+
+    public function findByName($name)
+    {
+        $user = new User();
+        $user->setName($name);
+        $user->setId($this->redis->get("username:$name:uid");
+
+        return $user;
+    }
 }
